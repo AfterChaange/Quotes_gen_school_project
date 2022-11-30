25b2b20436aba7435443a82bc855239f9365851e
AfterChaange <89050272+AfterChaange@users.noreply.github.com>
2022-12-01 00:57:45 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Copak to bude dnes?" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          jebne mě dneska už :!
         </p>
         <div class="neco">
           
