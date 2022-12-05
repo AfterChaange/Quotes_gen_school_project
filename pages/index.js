905d6b3648cad6c1eb0cc2bf37dc5f6cfcf76104
AfterChaange <89050272+AfterChaange@users.noreply.github.com>
2022-12-05 18:03:45 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>píčovina</title>
+        <title> quotes gen</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
